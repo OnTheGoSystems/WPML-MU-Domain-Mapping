26f9b3be411e9d7e70de7032e573a6aa718b9b33
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2017-06-21 16:22:59 -0300

    Remove all existing tests

--- tests/tests/inc/url-handling/test-url-converter.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-abstract class Test_URL_Converter extends WPML_UnitTestCase {
-	protected $languages_urls;
-}
--- tests/util/wpml-mu-domain-mapping-unittestcase.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class WPML_MU_Domain_Mapping_UnitTestCase extends WPML_UnitTestCase {
-
-}
