dfb0b6d6ac434fb873b97f593618591b6301afab
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2017-06-21 15:07:07 -0300

    [wpmlbrigde-94] Replace old WPML autoloader with simple require
    Also update version to 1.1.0
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlbridge-94

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+#1.1.0
+
+* [wpmlbridge-94] Replaced the old WPML autoloader with a simple require.
+
 #1.0.0
 
 * [wpmlcore-2647] The domain is now mapped for secondary languages
--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@ Plugin URI: https://wpml.org/
 Description: This is a bridge plugin to make <a href="https://wordpress.org/plugins/wordpress-mu-domain-mapping/" target="_blank">MU Domain Mapping</a> compatible with WPML
 Author: OnTheGoSystems
 Author URI: http://www.onthegosystems.com/
-Version: 1.0.0
+Version: 1.1.0
 Plugin Slug: wpml-mu-domain-mapping
 */
 
@@ -17,9 +17,7 @@ new WPML_MU_Domain_Mapping_Requirements();
 function wpml_mu_domain_mapping_enable() {
 	global $wpdb, $sitepress;
 
-	$wpml_auto_loader_instance = WPML_Auto_Loader::get_instance();
-	$wpml_auto_loader_instance->register( dirname( __FILE__ ) . '/' );
-
+	require_once 'classes/class-wpml-mu-domain-mapping-filters.php';
 	$mu_domain_mapping_filters = new WPML_MU_Domain_Mapping_Filters( $wpdb, $sitepress );
 	$mu_domain_mapping_filters->init_hooks();
 }
